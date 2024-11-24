5184731c449aae6e1d695d3498ca4aaf7b62ec06
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2024-11-24 11:36:45 -0500

    Add CheckerboardCopula import and update version to 0.0.3

--- discopula/__init__.py
+++ discopula/__init__.py
@@ -1,4 +1,8 @@
 """
   Discopula is a Python package for the estimation of copula-based models for discrete data. 
 """
-__version__ = "0.0.2"
+
+from discopula.checkerboard.copula import CheckerboardCopula
+
+__version__ = "0.0.3"
+__all__ = ["CheckerboardCopula"]
--- tests/test_checkerboard.py
+++ tests/test_checkerboard.py
@@ -1,5 +1,5 @@
 import numpy as np
-from discopula.checkerboard import CheckerboardCopula
+from discopula import CheckerboardCopula
 import pytest
 
 @pytest.fixture
