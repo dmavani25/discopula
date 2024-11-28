dd37acedf65c48906a3ecaa8c9580f3ebacec8d7
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2024-11-28 11:23:59 -0500

    Enhance documentation configuration: add source code linking, LaTeX output options, and update Sphinx extensions

--- docs/code.md
+++ docs/code.md
@@ -3,4 +3,5 @@
 ```{autodoc2-object} discopula.checkerboard.copula
 render_plugin = "myst"
 no_index = true
+show_source = true
 ```
--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = "discopula"
 copyright = "2024-2024, Dhyey Dharmendrakumar Mavani"
 author = "Dhyey Mavani"
@@ -13,7 +17,12 @@ author = "Dhyey Mavani"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "autodoc2"]
+extensions = [
+    "myst_parser",
+    "autodoc2",
+    "sphinx.ext.linkcode",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -28,5 +37,30 @@ autodoc2_packages = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-
 html_static_path = ["_static"]
+
+# -- Options for LaTeX output ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+}
+
+latex_documents = [
+    (
+        'index',
+        'discopula.tex',
+        'Discopula Documentation',
+        author,
+        'manual'
+    ),
+]
+
+# Add source code link configuration
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    filename = info['module'].replace('.', '/')
+    return f"https://github.com/dmavani25/discopula/blob/master/{filename}.py"
