3bf64871135a84f0217898f3749f173ab52579ab
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2024-11-29 14:44:24 -0500

    Refactor imports, improve code & tests organization, and add utility functions as necessary

--- discopula/__init__.py
+++ discopula/__init__.py
@@ -3,14 +3,16 @@
 """
 
 from discopula.checkerboard.copula import CheckerboardCopula
-from discopula.checkerboard.copula import contingency_to_case_form, case_form_to_contingency
-from discopula.checkerboard.copula import bootstrap_ccram, bootstrap_sccram
-from discopula.checkerboard.copula import bootstrap_regression_U1_on_U2, bootstrap_regression_U2_on_U1
-from discopula.checkerboard.copula import bootstrap_regression_U1_on_U2_vectorized, bootstrap_regression_U2_on_U1_vectorized
-from discopula.checkerboard.copula import bootstrap_predict_X2_from_X1, bootstrap_predict_X1_from_X2
-from discopula.checkerboard.copula import bootstrap_predict_X2_from_X1_vectorized, bootstrap_predict_X1_from_X2_vectorized
-from discopula.checkerboard.copula import bootstrap_predict_X1_from_X2_all_comb_summary, bootstrap_predict_X2_from_X1_all_comb_summary
-from discopula.checkerboard.copula import permutation_test_ccram, permutation_test_sccram
+from discopula.checkerboard.utils import contingency_to_case_form, case_form_to_contingency
+from discopula.checkerboard.statsim import (
+        bootstrap_ccram, bootstrap_sccram,
+        bootstrap_regression_U1_on_U2, bootstrap_regression_U2_on_U1,
+        bootstrap_regression_U1_on_U2_vectorized, bootstrap_regression_U2_on_U1_vectorized,
+        bootstrap_predict_X1_from_X2, bootstrap_predict_X2_from_X1,
+        bootstrap_predict_X1_from_X2_vectorized, bootstrap_predict_X2_from_X1_vectorized,
+        bootstrap_predict_X1_from_X2_all_comb_summary, bootstrap_predict_X2_from_X1_all_comb_summary,
+        permutation_test_ccram, permutation_test_sccram
+    )
 
 __version__ = "0.0.5"
 __all__ = [
