ef91b9c88006556a969317507205e5192f7a8f24
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-01-01 15:19:14 +0530

    Fix import order and update __all__ in __init__.py for consistency

--- discopula/__init__.py
+++ discopula/__init__.py
@@ -1,16 +1,16 @@
 """
   Discopula is a Python package for the estimation of copula-based models for discrete data. 
 """
+from discopula.checkerboard.gencopula import GenericCheckerboardCopula
 from discopula.checkerboard.utils import gen_contingency_to_case_form, gen_case_form_to_contingency
 from discopula.checkerboard.genstatsim import (
         bootstrap_ccram,
         permutation_test_ccram,
     )
-from discopula.checkerboard.gencopula import GenericCheckerboardCopula
 
 __version__ = "0.2.0"
 __all__ = [
-  "GenericCheckerboardCopula"
+  "GenericCheckerboardCopula",
   "gen_contingency_to_case_form",
   "gen_case_form_to_contingency",
   "bootstrap_ccram",
