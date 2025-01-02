03b0089eae2c89b58b324d3770ef03472c0f5fc4
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-01-02 14:07:54 +0530

    Bump version to 0.2.2; update examples to work for jupyter notebooks in accordance with generic implementations

--- discopula/__init__.py
+++ discopula/__init__.py
@@ -10,7 +10,7 @@ from discopula.checkerboard.genstatsim import (
         permutation_test_ccram,
     )
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = [
   "GenericCheckerboardCopula",
   "gen_contingency_to_case_form",
