d314871362629d578c9390c87b0e8d0333bbf1e4
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-02-12 00:23:22 -0500

    Bump version to 0.6.1 in __init__.py

--- discopula/__init__.py
+++ discopula/__init__.py
@@ -9,7 +9,7 @@ from discopula.checkerboard.genstatsim import (
         permutation_test_ccram,
     )
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __all__ = [
   "GenericCheckerboardCopula",
   "gen_contingency_to_case_form",
